8926955666a742e02e7912b6f99b3f981a25c0aa
Joel Santamaria Caila <kolonic.x@gmail.com>
2025-12-21 15:47:45 +0100

    Implement basic Todo service and tests, jacoco implementation

--- src/main/java/com/joel/todoapp/model/Todo.java
+++ src/main/java/com/joel/todoapp/model/Todo.java
@@ -1,16 +1,17 @@
 package com.joel.todoapp.model;
 
 import jakarta.persistence.*;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
 @Entity
-@Getter
-@Setter
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Data
+@Builder
 public class Todo {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
